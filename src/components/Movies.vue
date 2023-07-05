834b2bd75050ffa9c2cb38a461c16490d502cb40
Marc Droger <marcdroger@hotmail.com>
2023-07-05 23:57:31 +0200

    Added category filter

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -2,6 +2,15 @@
   <main>
     <div class="input">
       <input class="input__search" v-model="search" type="text" placeholder="Search film database" />
+      <p>Categories: </p>
+      <select class="input__category" v-model="category">
+        <option value="">All</option>
+        <option value="Action">Action</option>
+        <option value="Crime">Crime</option>
+        <option value="Drama">Drama</option>
+        <option value="History">History</option>
+        <option value="Western">Western</option>
+      </select>
     </div>
     <ul class="movies">
       <p v-if="!filterList.length" class="movies__empty">
@@ -20,7 +29,7 @@
           <img class="movies__close" src="/img/close.svg" alt="close popup" />
           <div class="movies__container">
             <h2 class="movies__title">{{ movie.title }}</h2>
-            <div class="movies__info"><p><span>Released</span> {{ movie.release }} </p><p><span>Length </span>{{ movie.length }} min.</p><p><span> Gross </span>${{ movie.boxoffice }}M</p></div>
+            <div class="movies__info"><p><span>Released</span> {{ movie.release }} </p><p><span> Category </span>{{ movie.category }}</p><p><span>Length </span>{{ movie.length }} min.</p><p><span> Gross </span>${{ movie.boxoffice }}M</p></div>
             <p class="movies__description"> {{ movie.description }}</p>
           </div>
         </div>
@@ -38,15 +47,14 @@
     data() {
       return {
         movies: json,
-        search: ''
+        search: '',
+        category: ''
       }
     },
 
     computed: {
       filterList() {
-        return this.movies.filter(movie => {
-          return movie.title.toLowerCase().includes(this.search.toLowerCase())
-        })
+        return this.movies.filter(movie => !movie.category.indexOf(this.category) && movie.title.toLowerCase().includes(this.search.toLocaleLowerCase()))
       }
     }
   }
--- src/scripts/togglePopup.js
+++ src/scripts/togglePopup.js
@@ -1,20 +1,24 @@
 export default function showPopup() {
+  //get all movie poster elements
   const moviePosters = document.querySelectorAll('.movies__front');
 
   moviePosters.forEach((poster) => {
+    //get popup element
     let popupElement = poster.nextElementSibling;
 
+    //get video element
     let videoElement = popupElement.childNodes[0];
 
+    //get close icon element
     let closeElement = popupElement.childNodes[1];
 
-    console.log(closeElement);
-
+    //add click event for poster and toggle popup + play video
     poster.addEventListener('click', () => {
       popupElement.classList.toggle('movies__popup--show');
       videoElement.play();
     })
 
+    //add click event for close icon and toggle popup + pause video
     closeElement.addEventListener('click', () => {
       popupElement.classList.toggle('movies__popup--show');
       videoElement.pause();
